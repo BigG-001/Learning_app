1bd0fd2a131617b10024aa7cd69be3b81f0d7111
Big G <guddysax@gmail.com>
2026-08-28 19:52:03 +0100

    Array-length, push()

---
+++ Flip-folder/function-array-objects.js
@@ -0,0 +1,10 @@
+// (1) ARRAY LENGTH
+// Array length tells one how many items are inside an array
+let subject = ["English", "Mathematics", "chemistry"];
+console.log(subject.length);
+
+// (2) push(); 
+// It adds an item at the end of an array.
+let food = ["Rice", "Beans"];
+food.push("Soup");
+console.log(food); 
