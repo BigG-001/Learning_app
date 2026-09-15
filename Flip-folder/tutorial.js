c0916ee24f5009a6ca59103ec451703f11c9061b
Big G <guddysax@gmail.com>
2026-09-15 22:08:46 +0100

    Form validation and password validation

---
+++ Flip-folder/tutorial.js
@@ -0,0 +1,18 @@
+function greet() {
+  // code here
+}
+const greetings = () => {};
+
+// if 
+// else if 
+// else
+
+let age1 = 20;
+
+
+if (age1 <= 18){
+  console.log("Qualified to vote");
+}else{
+  console.log("not qualified to vote")
+}
+// == === <== < > >== !==
